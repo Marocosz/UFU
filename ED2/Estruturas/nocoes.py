de0591adf2bcaf8c98d04972a8ec0f0ca3259ea3
Marcos Rodrigues <85518327+Marocosz@users.noreply.github.com>
2024-02-17 18:27:58 -0300

    17/02 1

---
+++ ED2/Estruturas/nocoes.py
@@ -0,0 +1,8 @@
+"""
+Complexidade: É a medida de eficiência de um algorítmo 
+
+Algorpítmo: Um algorítmo tem dois grandes fatores para determinar sua eficiência, sendo elas:
+
+Complexidade de Espaço (Memória) -> Quantidade de variáveis, elementos (brutamente falando)
+Complexidade de Tempo (Processamento) -> Quantidade de operações elementares do algorítmo (atribuções, cálculos, ...)
+"""
