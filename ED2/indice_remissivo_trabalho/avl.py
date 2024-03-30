4b8e73b399140e250ea0412703c159284a7cb313
Marcos Rodrigues <85518327+Marocosz@users.noreply.github.com>
2024-03-29 21:54:22 -0300

    29/03 - 1

--- ED2/indice_remissivo_trabalho/avl.py
+++ ED2/indice_remissivo_trabalho/avl.py
@@ -73,6 +73,14 @@ class AVL:
 
         return False
 
+    def searchMin(self, current):
+        node1 = current
+        node2 = current.left
+        while node2 is not None:
+            node1 = node2
+            node2 = node2.left
+        return node1
+
     def __insert(self, elem, current):
         if current is None:
             new = Node(elem)
@@ -105,4 +113,100 @@ class AVL:
             self.root = self.__insert(self.root, elem)
             return True
 
+    def __remove(self, current, valor):
 
+        if current.info == valor: # Achou o nó a se removido
+            if current.left is None or current.left is None: # Há apenas um filho
+                if current.left is None:
+                    current = current.left
+                else:
+                    current = current.right
+
+            else: # Há dois filhos
+                temp = self.searchMin(current.right)
+                current.data = temp.data
+                current.right = self.__remove(current.right, current.data)
+
+                if self.balancingFactor(current) >= 2:
+                    if self.__height(current.left.right) <= self.__height(current.left.right):
+                        current = self.rotationLL(current)
+                    else:
+                        current = self.rotationLR(current)
+
+            if current is not None:
+                current.height = self.bigger(self.__height(current.left), self.__height(current.right)) + 1
+
+        else: # Procurará o nó a ser removido
+            if valor < current.data:
+                current.left = self.__remove(current.left, valor)
+
+                if self.balancingFactor(current) >= 2:
+                    if self.__height(current.right.left) <= self.__height(current.right.right):
+                        current = self.rotationRR(current)
+                    else:
+                        current = self.rotationRL(current)
+
+            else:
+                current.right = self.__remove(current.right, valor)
+
+                if self.balancingFactor(current) >= 2:
+                    if self.__height(current.left.right) <= self.__height(current.left.left):
+                        current = self.rotationLL(current)
+                    else:
+                        current = self.rotationLR(current)
+
+            current.height =
+
+    def remove(self, elem):
+        if self.root is None or not self.search(elem):
+            # árvore não existe ou item não existe
+            return False
+        else:
+            self.root = self.__remove(self.root, elem)
+            return True
+
+    def preOrder(self, root):
+        if root is not None:
+            print(root.data)
+            self.preOrder(root.left)
+            self.preOrder(root.right)
+
+    def inOrder(self, root):
+        if root is not None:
+            self.inOrder(root.left)
+            print(root.data, end='')
+            self.inOrder(root.right)
+
+    def postOrder(self, root):
+        if root is not None:
+            self.postOrder(root.left)
+            self.postOrder(root.right)
+            print(root)
+
+    def printLevel(self, root, level):
+        if root is None:
+            return 0
+        if level == 0:
+            print(root.data, end=' ')
+        elif level > 0:
+            self.printLevel(root.left, level-1)
+            self.printLevel(root.right, level-1)
+
+    def inLevel(self):
+        h = self.__height(self.root)
+        for i in range(0, h+1):
+            self.printLevel(self.root, i)
+            print(f'{i}º Level')
+
+    def __changeValueNode(self, root):
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return root.data
+        else:
+            root.data = self.__changeValueNode(root.left) + self.__changeValueNode(root.right)
+            return root.data
+
+    def changeValueNode(self):
+        if self.root is not None:
+            self.__changeValueNode(self.root)
