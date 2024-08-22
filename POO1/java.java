03fd37fe6d9d3ec5d2d9f13715069393ff3e031d
Marocos <ditanixplayer@gmail.com>
2024-08-21 23:19:23 -0300

    att-21-08

--- POO1/java.java
+++ POO1/java.java
@@ -1,1 +1,20 @@
 // Aula 01 
+
+import java.util.Scanner;
+
+public class Soma {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Digite o valor do primeiro número: ");
+        int num1 = entrada.nextInt();
+        
+        System.out.print("Digite o valor do segundo número: ");
+        int num2 = entrada.nextInt();
+        
+        int soma = num1 + num2;
+        
+        System.out.println("A soma dos dois números é: " + soma);
+    }
+}
+
