fa46e2cc6e48f006e249d418c9791d6770ee4fa9
Marocosz <ditanixplayer@gmail.com>
2024-11-05 01:32:11 -0300

    Commit 05-11
    Att 5/11

---
+++ POO1/Prova3_Estudo/interface_colecoes.java
@@ -0,0 +1,17 @@
+public class interface_colecoes {
+
+    /*
+     *  Interface: Conjunto de métodos disponíveis em um objeto
+     *  Em outras palavras, uma interface define quais métodos que uma classe deve implementar visando tais funcionalidades
+     * 
+     * A interface diz oque o objeto deve fazer, mas não como fazer. Isto será definido na implementação da interface por uma classe
+     * (palavra reservada: implements)
+     * 
+     * Interfaces também podem herdar carcterísticas de outras interfaces e somente interfaces
+     * 
+     * 
+     * 
+     * 
+     */
+    
+}
---
+++ POO1/Prova3_Estudo/polimorfismo.java
@@ -0,0 +1,29 @@
+public class polimorfismo {
+    /*
+     *  Polimorfismo vem de poli (muitas) morfis (formas) ou seja, muitas formas
+     * 
+     * É a habilidade de objetos de uma ou mais classes em responder a uma mesma mensagem de formas diferentes
+     * 
+     * Há duas maneiras de ocorrer: Sobrecarga (Overloading)
+     * e: Sobreescrita (Overrding)
+     * 
+     * 
+     * Existe a "Assinatura" de um método, que é composto pelo seu nome e pelos tipos de seus argumentos, independente dos nomes dos argumentos e do valor de retorno
+     * 
+     * E a sobrecarga é justamente criar um método com mesmo nome porém com tipos de argumentos diferentes ou quantidades de argumentos diferentes
+     * (Polimorfismo Estático)
+     * 
+     * Agora, na sobreescrita, a classe derivada tem uma função com a mesma assinatura porém um funcionamento diferente
+     * O método  da classe derivada sobrepõe a função da classe base.
+     * (Polimorfismo Dinâmico)
+     * 
+     * Classe Abstrata (ou classe virtual) é uma classe incompleta onde alguns ou todos os seus métodos não possuem implementação
+     * Elas não podem ser instanciadas, servem apenas para reunir características comuns dos descendentes
+     * Palavra reservada abstract
+     * 
+     * Na UML a classe abstrata é escrito em itálico
+     * 
+     */
+
+
+}
